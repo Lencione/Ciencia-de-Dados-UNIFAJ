672a5dd81a17589a61c878ab0a0798fd960f161d
Wesley <wesley@lencione.com.br>
2024-04-06 16:28:57 -0300

    Added second avaliation

---
+++ Avaliation/Avaliation.py
@@ -0,0 +1,55 @@
+from sklearn.neural_network import MLPClassifier
+from coffees_db import roast as roast_db
+from coffees_db import harvest as harvest_db
+from coffees_db import localization as localization_db
+from coffees_db import coffee as coffee_db
+
+
+def train_model(model_conditions, db):
+    x = []
+    y = []
+    for model in db:
+        for condition in model["conditions"]:
+            x.append([condition[model_condition] for model_condition in model_conditions])
+            y.append(model["quality"])
+    model = MLPClassifier(solver='lbfgs', activation='logistic', alpha=1e-8, hidden_layer_sizes=(450, 450),
+                          random_state=1, verbose=False)
+    model.fit(x, y)
+    return model
+
+
+def get_answers(conditions):
+    return [int(input(f"What is {condition}? (Min: 1 / Max: 7)")) for condition in conditions]
+
+
+def predict_model(model, answers):
+    return model.predict([answers])[0]
+
+
+
+
+
+harvest_conditions = ["height", "maturity"]
+roast_conditions = ["size", "temperature", "time"]
+localization_conditions = ["latitude", "longitude"]
+coffee_conditions = ["roast", "harvest", "localization"]
+
+harvest_model = train_model(harvest_conditions, harvest_db)
+roast_model = train_model(roast_conditions, roast_db)
+localization_model = train_model(localization_conditions, localization_db)
+coffee_model = train_model(coffee_conditions, coffee_db)
+
+print("Answer the questions about the Harvest of coffee:")
+harvest_answer = predict_model(harvest_model, get_answers(harvest_conditions))
+
+print("\n\nAnswer the questions about the Roast of coffee:")
+roast_answer = predict_model(roast_model, get_answers(roast_conditions))
+
+print("\n\nAnswer the questions about the Localization of coffee:")
+localization_answer = predict_model(localization_model, get_answers(localization_conditions))
+
+coffee_answer = predict_model(coffee_model, [harvest_answer, roast_answer, localization_answer])
+
+print("\n\nThe quality of the coffee is: (MAX: 7 | MIN:1)")
+print(coffee_answer)
+
---
+++ Avaliation/coffees_db.py
@@ -0,0 +1,320 @@
+harvest = [
+    {
+        "quality": 1,
+        "conditions": [
+            {
+                "height": 1,
+                "maturity": 1,
+            },
+            {
+                "height": 2,
+                "maturity": 1,
+            },
+            {
+                "height": 1,
+                "maturity": 2,
+            }
+        ]
+    },
+    {
+        "quality": 2,
+        "conditions": [
+            {
+                "height": 2,
+                "maturity": 2,
+            },
+            {
+                "height": 3,
+                "maturity": 2,
+            },
+            {
+                "height": 2,
+                "maturity": 3,
+            }
+        ]
+    },
+
+    {
+        "quality": 3,
+        "conditions": [
+            {
+                "height": 3,
+                "maturity": 3,
+            },
+            {
+                "height": 4,
+                "maturity": 3,
+            },
+            {
+                "height": 3,
+                "maturity": 4,
+            }
+        ]
+    },
+    {
+        "quality": 4,
+        "conditions": [
+            {
+                "height": 4,
+                "maturity": 4,
+            },
+            {
+                "height": 5,
+                "maturity": 4,
+            },
+            {
+                "height": 4,
+                "maturity": 5,
+            }
+        ]
+    },
+    {
+        "quality": 5,
+        "conditions": [
+            {
+                "height": 5,
+                "maturity": 5,
+            },
+            {
+                "height": 6,
+                "maturity": 5,
+            },
+            {
+                "height": 5,
+                "maturity": 6,
+            }
+        ]
+    },
+    {
+        "quality": 6,
+        "conditions": [
+            {
+                "height": 6,
+                "maturity": 6,
+            },
+            {
+                "height": 7,
+                "maturity": 6,
+            },
+            {
+                "height": 6,
+                "maturity": 7,
+            }
+        ]
+    },
+    {
+        "quality": 7,
+        "conditions": [
+            {
+                "height": 7,
+                "maturity": 7,
+            },
+            {
+                "height": 8,
+                "maturity": 7,
+            },
+            {
+                "height": 7,
+                "maturity": 8,
+            }
+        ]
+    }
+]
+
+roast = [
+    {
+        "quality": 1,
+        "conditions": [
+            {
+                "size": 1,
+                "temperature": 7,
+                "time": 7,
+            },
+            {
+                "size": 2,
+                "temperature": 7,
+                "time": 7,
+            },
+            {
+                "size": 3,
+                "temperature": 7,
+                "time": 7,
+            }
+        ]
+    },
+    {
+        "quality": 2,
+        "conditions": [
+            {
+                "size": 1,
+                "temperature": 7,
+                "time": 6,
+            },
+            {
+                "size": 2,
+                "temperature": 7,
+                "time": 6,
+            },
+            {
+                "size": 3,
+                "temperature": 6,
+                "time": 7,
+            }
+        ]
+    },
+    {
+        "quality": 3,
+        "conditions": [
+            {
+                "size": 2,
+                "temperature": 6,
+                "time": 6,
+            },
+            {
+                "size": 3,
+                "temperature": 5,
+                "time": 7,
+            },
+            {
+                "size": 4,
+                "temperature": 5,
+                "time": 6,
+            }
+        ]
+    },
+    {
+        "quality": 4,
+        "conditions": [
+            {
+                "size": 3,
+                "temperature": 5,
+                "time": 6,
+            },
+            {
+                "size": 4,
+                "temperature": 4,
+                "time": 7,
+            },
+            {
+                "size": 5,
+                "temperature": 4,
+                "time": 6,
+            }
+        ]
+    },
+    {
+        "quality": 7,
+        "conditions": [
+            {
+                "size": 7,
+                "temperature": 4,
+                "time": 6,
+            },
+            {
+                "size": 7,
+                "temperature": 5,
+                "time": 5,
+            },
+            {
+                "size": 7,
+                "temperature": 6,
+                "time": 4,
+            }
+        ]
+    }
+]
+
+localization = [
+    {
+        "quality": 1,
+        "conditions": [
+            {
+                "latitude": 1,
+                "longitude": 1,
+            },
+        ]
+    },
+    {
+        "quality": 2,
+        "conditions": [
+            {
+                "latitude": 2,
+                "longitude": 2,
+            },
+
+        ]
+    },
+    {
+        "quality": 7,
+        "conditions": [
+            {
+                "latitude": 7,
+                "longitude": 7,
+            },
+
+        ]
+    },
+]
+
+coffee = [
+    {
+        "quality": 1,
+        "conditions": [
+            {
+                "roast": 1,
+                "harvest": 1,
+                "localization": 1,
+            },
+            {
+                "roast": 2,
+                "harvest": 1,
+                "localization": 1,
+            },
+            {
+                "roast": 1,
+                "harvest": 3,
+                "localization": 3,
+            }
+        ]
+    },
+    {
+        "quality": 2,
+        "conditions": [
+            {
+                "roast": 2,
+                "harvest": 2,
+                "localization": 2,
+            },
+            {
+                "roast": 3,
+                "harvest": 2,
+                "localization": 2,
+            },
+            {
+                "roast": 2,
+                "harvest": 4,
+                "localization": 4,
+            }
+        ]
+    },
+    {
+        "quality": 7,
+        "conditions": [
+            {
+                "roast": 7,
+                "harvest": 7,
+                "localization": 7,
+            },
+            {
+                "roast": 7,
+                "harvest": 7,
+                "localization": 7,
+            },
+            {
+                "roast": 7,
+                "harvest": 7,
+                "localization": 7,
+            }
+        ]
+    }
+]
